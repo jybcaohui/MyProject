a28c6502abacd40f83101a9e37f3f98f61918714
qichengcaohui <caohui@qicheng.tv>
2015-10-28 18:44:12 +0800

    图文并茂的Edittext，修改

--- app/src/main/java/com/example/kr/myproject/edittextwhitimageandclick/EditTextActivity.java
+++ app/src/main/java/com/example/kr/myproject/edittextwhitimageandclick/EditTextActivity.java
@@ -12,7 +12,9 @@ import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.ImageSpan;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,7 +24,7 @@ import com.example.kr.myproject.R;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class EditTextActivity extends BaseActivity {
+public class EditTextActivity extends BaseActivity implements View.OnClickListener{
 
     //http://2711082222.blog.163.com/blog/static/106302249201232633333181/
     @InjectView(R.id.edt)
@@ -31,14 +33,19 @@ public class EditTextActivity extends BaseActivity {
     EditText edt2;
     @InjectView(R.id.edt3)
     EditText edt3;
+    @InjectView(R.id.edt4)
+    EditText text4;
+    @InjectView(R.id.but)
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
         ButterKnife.inject(this);
+        but.setOnClickListener(this);
         setPrice("#lable1#", "#lable2#");
         addImg();
-        clickSpan();
+        clickSpanImg();
     }
     public void setPrice(String m, String n) {
         String one = "共需";
@@ -95,8 +102,83 @@ public class EditTextActivity extends BaseActivity {
             }
         };
         spStr.setSpan(clickSpan, 0, spStr.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        edt3.append("\n");
         edt3.append(spStr);
+        edt3.append("\n");
         edt3.setMovementMethod(LinkMovementMethod.getInstance());//设置超链接为可点击状态
     }
 
+    public void clickSpanImg(){
+        SpannableString spStr = new SpannableString("http://www.news/090.jpg");
+        ClickableSpan clickSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                toast("http://www.news/090.jpg");
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                ds.setColor(Color.RED);//设置连接的文本颜色
+                ds.setUnderlineText(false); //去掉下划线
+            }
+        };
+        spStr.setSpan(clickSpan, 0, spStr.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        //第一张图片
+        Drawable drawable = getResources().getDrawable(R.drawable.pic);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight());
+        SpannableString spannable = new SpannableString(text4.getText()
+                .toString() + "[pic]");
+        ImageSpan span = new ImageSpan(drawable, ImageSpan.ALIGN_BASELINE);
+        spannable.setSpan(span, text4.getText()
+                        .length(),
+                text4.getText()
+                        .length() + "[pic]".length(),
+                Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+
+        text4.append("\n");
+        text4.append(spannable);
+        text4.append(spStr);
+
+        //第二张图片
+        Drawable drawable2 = getResources().getDrawable(R.drawable.cancel);
+        drawable2.setBounds(0, 0, drawable2.getIntrinsicWidth(),
+                drawable2.getIntrinsicHeight());
+        SpannableString spannable2 = new SpannableString("[cancel]");
+        ImageSpan span2 = new ImageSpan(drawable2, ImageSpan.ALIGN_BASELINE);
+//        ClickableSpan clickSpan2 = new ClickableSpan() {
+//            @Override
+//            public void onClick(View widget) {
+//                toast("测试可点击的EditText");
+//            }
+//
+//            @Override
+//            public void updateDrawState(TextPaint ds) {
+//                ds.setColor(Color.RED);//设置连接的文本颜色
+//                ds.setUnderlineText(false); //去掉下划线
+//            }
+//        };
+//        spannable2.setSpan(clickSpan2, 0,
+//                "[cancel]".length(),
+//                Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        spannable2.setSpan(span2, 0,
+                "[cancel]".length(),
+                Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        text4.append(spannable2);
+        text4.append("\n");
+        Log.d("text4---",text4.getText().toString());
+//        text4.setMovementMethod(LinkMovementMethod.getInstance());//设置超链接为可点击状态
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.but:
+                clickSpan();
+                Log.d("text---",edt3.getText().toString());
+                break;
+        }
+    }
 }
