2da9377ff090455d6138c17c022e0b740e6a9f19
caohui <caohui@qicheng.tv>
2016-02-02 11:43:28 +0800

    滚动广告栏

---
+++ app/src/main/java/com/example/kr/myproject/advertscrollbar/AdvertBarActivity.java
@@ -0,0 +1,16 @@
+package com.example.kr.myproject.advertscrollbar;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.kr.myproject.R;
+
+public class AdvertBarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advert_bar);
+    }
+
+}
